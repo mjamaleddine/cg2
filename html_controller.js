c1121fcaaabf15fd9e355c27d8711d8dc3311b95
Hagen Handtke <lustigepepe@web.de>
2013-11-03 01:21:34 +0100

    nicht fertig

--- html_controller.js
+++ html_controller.js
@@ -11,8 +11,8 @@
 
 
 /* requireJS module definition */
-define(["jquery", "straight_line", "circle_line"],
-        (function($, StraightLine, CircleLine) {
+define(["jquery", "straight_line", "circle_line", "parametric_curves"],
+        (function($, StraightLine, CircleLine, ParaCurve) {
 
             "use strict";
             /*
@@ -20,6 +20,23 @@ define(["jquery", "straight_line", "circle_line"],
              * and provide them with a closure defining context and scene
              */
             var HtmlController = function(context, scene, sceneController) {
+
+
+                // random min_t and max_t
+                var randomMinMax_t = function() {
+                    var mini = Math.floor(Math.random() * 50);
+                    var maxi = Math.floor(Math.random() * 50);
+                    var t_min = Math.min(mini, maxi);
+                    var t_max = Math.max(mini, maxi);
+
+                    return [t_min, t_max];
+                };
+
+                // random amount of segments 
+                var randomIntValue = function(min, max) {
+                    var delta = max - min;
+                    return (Math.floor(Math.random() * delta)) + min;
+                };
 
                 // generate random X coordinate within the canvas
                 var randomX = function() {
@@ -59,7 +76,7 @@ define(["jquery", "straight_line", "circle_line"],
                         width: Math.floor(Math.random() * 3) + 1,
                         color: randomColor()
                     };
-                    var circle = new CircleLine([randomX(), randomY()], randomR(), style);
+                    var circle = new CircleLine([randomX(), randomY()], parseInt(randomR()), style);
                     scene.addObjects([circle]);
                     // deselect all objects, then select the newly created object
                     sceneController.deselect();
@@ -85,6 +102,34 @@ define(["jquery", "straight_line", "circle_line"],
 
 
                 }));
+
+                /*
+                 * event handler for "parametric curve button".
+                 */
+                $("#btnNewParaCurve").click((function() {
+
+                    // create the actual line and add it to the scene
+                    var style = {
+                        width: Math.floor(Math.random() * 3) + 1,
+                        color: randomColor()
+                    };
+
+                    var x = randomX();
+                    var y = randomY();
+
+                    var minMax_t = randomMinMax_t();
+                    var f_t = x + "+100*Math.sin(t);";
+                    var g_t = y + "+100*Math.cos(t);";
+
+                    var paraCurve = new ParaCurve(minMax_t[0], minMax_t[1], f_t, g_t, randomIntValue(5, 30), style);
+                    console.log(paraCurve);
+                    scene.addObjects([paraCurve]);
+
+                    // deselect all objects, then select the newly created object
+                    sceneController.deselect();
+                    sceneController.select(paraCurve); // this will also redraw
+
+                }));
                 /*
                  *show properties of selected Object		
                  */
@@ -96,7 +141,10 @@ define(["jquery", "straight_line", "circle_line"],
                         $("#circleCircle").show();
                     }
                     ;
-
+                    if (obj instanceof ParaCurve) {
+                        $("#paraCurveProps").show();
+                    }
+                    ;
 
                     $(".input").change(getInputField);
                     $(".newProps").change(getInputField);
@@ -109,11 +157,24 @@ define(["jquery", "straight_line", "circle_line"],
 
                     var obj = sceneController.getSelectedObject();
                     obj.lineStyle.color = ($("#ipColor").attr("value"));
-                    obj.lineStyle.width = parseFloat($("#ipLineWidth").attr("value"));
+                    obj.lineStyle.width = parseInt($("#ipLineWidth").attr("value"));
 
                     if (obj instanceof CircleLine) {
                         console.log("Hallo");
                         obj.r = parseFloat($("#circRadius").attr("value"));
+                    }
+                    ;
+                    if (obj instanceof ParaCurve) {
+
+                        obj.min_t = parseFloat($("#ipParaCurveMinT").attr("value"));
+                        obj.max_t = parseFloat($("#ipParaCurveMaxT").attr("value"));
+                        obj.segment = parseFloat($("#ipParaCurveSeg").attr("value"));
+                        obj.f_t = $("#ipParaCurveX_t").attr("value");
+                        obj.g_t = $("#ipParaCurveY_t").attr("value");
+                        obj.tickMarks = $("#cbParaCurve").is(':checked');
+
+                        console.log(obj.f_t + " \n" + obj.g_t);
+
                     }
                     ;
                     sceneController.scene.draw(context);
@@ -131,6 +192,17 @@ define(["jquery", "straight_line", "circle_line"],
                         $("#circRadius").attr("value", parseFloat(obj.r));
                     }
                     ;
+                    if (obj instanceof ParaCurve) {
+
+                        $("#ipParaCurveMinT").attr("value", parseFloat(obj.min_t));
+                        $("#ipParaCurveMaxT").attr("value", parseFloat(obj.max_t));
+                        $("#ipParaCurveSeg").attr("value", parseFloat(obj.segment));
+                        $("#ipParaCurveX_T").attr("value", obj.f);
+                        $("#ipParaCurveY_T").attr("value", obj.g);
+
+                    }
+                    ;
+
                 };
                 sceneController.onSelection(showSelectedProps);
                 sceneController.onObjChange(setInputField);
